cac783af20a9f316e148f6110bb0f90b678f76a3
Nick <nikheal25@gmail.com>
2020-06-28 17:41:05 +1000

    Initial Commit

---
+++ Dummy/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Dummy
+//
+//  Created by Nick Gholap on 28/6/20.
+//  Copyright © 2020 Nick Gholap. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
