2b73c8a06f46052c796deccd82ab013d5e70e74a
Nick <nikheal25@gmail.com>
2020-06-28 17:43:15 +1000

    first

--- Dummy/ContentView.swift
+++ Dummy/ContentView.swift
@@ -10,8 +10,16 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                Text("Hello")
+            }
+            Text("Hello, World!")
+        }
+    
     }
+    
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
